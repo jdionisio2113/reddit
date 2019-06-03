060255af04c0b2966fae2f75bd9d27190692ed1d
Justin Dionisio <justindionisio21@yahoo.com>
2019-06-02 20:32:04 -0700

    Created separate file for the store and rendered <Root/> in index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
-import { createStore, applyMiddleware } from "redux";
-import { selectSubreddit, fetchPosts } from "./actions";
-import rootReducer from "./reducers";
 import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
+import Root from "./containers/Root";
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware, // disptch() functions
-    loggerMiddleware // middleware that logs actions
-  )
-);
-
-store.dispatch(selectSubreddit("reactjs"));
-store.dispatch(fetchPosts("reactjs")).then(() => console.log(store.getState));
+render(<Root />, document.getElementById("root"));
